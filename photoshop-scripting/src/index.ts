be9ee4af2fbe768454704a97b96c8d172192161a
heyitaki <akshath@berkeley.edu>
2021-07-12 00:16:49 -0700

    transpile ts to es3 before running

--- photoshop-scripting/src/index.ts
+++ photoshop-scripting/src/index.ts
@@ -1,16 +1,16 @@
 const allLayers: string[] = [];
 
-function collectAllLayers(parent: Document, level: number) {
+const collectAllLayers = (parent: Document, level: number) => {
   for (var m = parent.layers.length - 1; m >= 0; m--) {
-    var layer: Layer = parent.layers[m];
+    const layer: Layer = parent.layers[m];
     if (layer.typename !== "ArtLayer") {
-      allLayers.push(level + layer.name);
+      allLayers.push(level + " " + layer.name);
       collectAllLayers(layer as any as Document, level + 1);
     } else {
       $.writeln(layer.name);
     }
   }
-}
+};
 
 collectAllLayers(app.activeDocument, 0);
 $.writeln(allLayers);
