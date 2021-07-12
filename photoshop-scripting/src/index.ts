1f7a0b3ab64d3a84f280d5ffe576047cd3a4fd6a
heyitaki <akshath@berkeley.edu>
2021-07-11 17:12:04 -0700

    fix type import conflict

--- photoshop-scripting/src/index.ts
+++ photoshop-scripting/src/index.ts
@@ -1,11 +1,11 @@
-const allLayers = [];
+const allLayers: string[] = [];
 
-function collectAllLayers(parent, level) {
+function collectAllLayers(parent: Document, level: number) {
   for (var m = parent.layers.length - 1; m >= 0; m--) {
-    var layer = parent.layers[m];
+    var layer: Layer = parent.layers[m];
     if (layer.typename !== "ArtLayer") {
       allLayers.push(level + layer.name);
-      collectAllLayers(layer, level + 1);
+      collectAllLayers(layer as any as Document, level + 1);
     } else {
       $.writeln(layer.name);
     }
