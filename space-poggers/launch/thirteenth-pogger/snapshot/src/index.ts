8f5d78de48adbf72772241549d7f55aea8d0e939
heyitaki <akshath@berkeley.edu>
2021-08-26 22:25:19 -0700

    added all token metadata

--- space-poggers/launch/thirteenth-pogger/snapshot/src/index.ts
+++ space-poggers/launch/thirteenth-pogger/snapshot/src/index.ts
@@ -1,10 +1,11 @@
-import { enumerateTokenHolders } from './enumerate';
-import { getTransfersManually } from './transfers';
+import { enumerateTokenHolders, getTotalNumCompletedSets } from './enumerate';
+import { getAllTokenMetadata } from './metadata';
 
 const takeSnapshot = async () => {
   const timestamp = Date.now();
-  await getTransfersManually(12983740);
+  // await getTransfersManually(12983740);
   await enumerateTokenHolders();
+  await getTotalNumCompletedSets();
 };
 
-takeSnapshot();
+getAllTokenMetadata();
--- space-poggers/launch/thirteenth-pogger/snapshot/src/metadata.ts
+++ space-poggers/launch/thirteenth-pogger/snapshot/src/metadata.ts
@@ -1,8 +1,11 @@
 import fs from 'fs';
 
+type IpfsMetadata = { attributes: { trait_type: string; value: string }; image: string };
+
 const MAX_SUPPLY = 12000;
 
 const getOffsetId = (tokenId: number) => (tokenId + 567) % MAX_SUPPLY;
+const getTokenId = (offsetId: number) => (offsetId - 567 + MAX_SUPPLY) % MAX_SUPPLY;
 
 const getTokenTribe = (tokenId: number) => {
   return fs.promises
@@ -10,7 +13,7 @@ const getTokenTribe = (tokenId: number) => {
     .then((data) => JSON.parse(data.toString()).attributes[0].value);
 };
 
-export const getAllTokenMetadata = async () => {
+export const getAllTokenTribes = async () => {
   const metadata = new Array(MAX_SUPPLY);
   const BATCH_SIZE = 1000;
   for (let i = 0; i < Math.ceil(MAX_SUPPLY / BATCH_SIZE); i++) {
@@ -79,4 +82,38 @@ export const enumerateTribeCounts = (tribeList: string[]) => {
   }
 
   return counts;
+};
+
+export const getAllTokenMetadata = () => {
+  const allMetadata: { [key: number]: IpfsMetadata } = {};
+
+  // Normal Poggers
+  const p1 = fs.promises.readdir('../../photoshop-scripting/v2/Metadata').then((files) => {
+    console.log(`Found ${files.length} files...`);
+    for (let i = 0; i < files.length; i++) {
+      const file = files[i];
+      const tokenId = getTokenId(parseInt(file));
+      const data = fs.readFileSync(`../../photoshop-scripting/v2/Metadata/${file}`);
+      allMetadata[tokenId] = JSON.parse(data.toString());
+      console.log(`(${i + 1}/${files.length}) Grabbed token metadata: ${tokenId}`);
+    }
+  });
+
+  // 13th Pogger
+  const p2 = fs.promises.readdir('../photoshop-scripting/Metadata').then((files) => {
+    console.log(`Found ${files.length} files...`);
+    for (let i = 0; i < files.length; i++) {
+      const tokenId = parseInt(files[i]);
+      const data = fs.readFileSync(`../photoshop-scripting/Metadata/${tokenId}`);
+      allMetadata[tokenId] = JSON.parse(data.toString());
+      console.log(`(${i + 1}/${files.length}) Grabbed token metadata: ${tokenId}`);
+    }
+  });
+
+  return Promise.all([p1])
+    .then(() => console.log(`Processed ${Object.keys(allMetadata).length} tokens`))
+    .then(() =>
+      fs.promises.writeFile('./data/all_token_metadata.json', JSON.stringify(allMetadata)),
+    )
+    .then(() => console.log('Wrote all token metadata to file'));
 };
