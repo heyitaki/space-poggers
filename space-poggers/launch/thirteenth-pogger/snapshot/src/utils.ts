f721141b1ee87c8fbb1a01d60671364239a2f6c7
heyitaki <akshath@berkeley.edu>
2021-09-02 08:31:42 -0700

    finish showcase contest snapshot

--- space-poggers/launch/thirteenth-pogger/snapshot/src/utils.ts
+++ space-poggers/launch/thirteenth-pogger/snapshot/src/utils.ts
@@ -1,6 +1,10 @@
-export const numToHexStr = (num?: number) => {
+export const numToHex = (num?: number) => {
   if (!num) return;
   return '0x' + num.toString(16);
+};
+
+export const hexToNum = (hex: string) => {
+  return parseInt(hex);
 };
 
 export const sleep = (ms: number) => {
